2ee082bdd7813be33226c1cbb51594e75707be31
Felix <bpr59@comcast.net>
2020-07-17 20:23:43 -0400

    new server file with all comments

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
+// Node modules used in app development
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
 const bodyParser = require("body-parser");
-// const { notStrictEqual } = require("assert");
 
 const log = console.log;
 
+//middleware before executing any end route logic 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
 app.use(bodyParser());
 
+//declaration of database file and read
 let dataBase = fs.readFileSync("./db/db.json","utf-8");
-
-// log("database:", dataBase)
 
 dataBase ? dataBase = JSON.parse(dataBase) : dataBase = [];
 
+//acquiring information from html files
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
@@ -30,6 +31,7 @@ app.get("/api/notes", (req, res) => {
     return res.send(dataBase);
   });
 
+//returning information received via app.get
 app.post('/api/notes', (req, res) => {
     let body = req.body;
     let noteId = {"id":Math.round(Math.random()* 9999)};
@@ -39,8 +41,7 @@ app.post('/api/notes', (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(dataBase), "utf-8");
   });
 
-// ----------------------------------------------------------
-
+//function to delete note from sidebar list
 app.delete("/api/notes/:id", (req, res) => {
   
   let id = req.params.id;
@@ -50,15 +51,16 @@ app.delete("/api/notes/:id", (req, res) => {
 
       if(notes[i].id == id){
           notes.splice(i, 1);
-          res.send("deleted");
+          res.send("The note was deleted");
       }
   }
   fs.writeFileSync("./db/db.json", JSON.stringify(dataBase), "utf-8");
 });
-// ----------------------------------------------------------
 
+//response of 404 file to user of localhost query to a wrong link
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/error.html'));  
 })
 
+//localhost listening link and response to user
 app.listen(8080, () => log("App is running at port ", 8080));
